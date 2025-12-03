4c4335c231672ddeb4f8cccce624de8de22e1347
Maurilio Nava Junior <mnavajunior@gmail.com>
2025-12-02 23:55:52 -0300

    Add introductory class and summary documentation for .NET fundamentals

---
+++ CsharpFundamentos/PraticaDez25/S01-Intro.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// A plataforma .NET atual fornece um conjunto padrão de bibliotecas de classes básicas e APIs comuns a todos os aplicativos .NET
+/// Multiplataforma através da CLR
+/// Compilação para a IL(linguagem intermediária - bytecode) .exe ou .dll(Dynamic Link Library), posterior execução através da CLR(common language runtime) com JIT
+/// Organização do código: assembly, classe, namespace = container de classes, solução = container de projetos, 
+/// Interoperabilidade?
+/// </summary>
+public class Intro
+{
+}
